23dbbdc17d955a307839f0261cf659cab889db68
Eray Kısabacak <nefret19981998@hotmail.com>
2019-12-02 17:09:32 +0300

    C# ile Dosya Okuyarak Telefon Rehberi Uygulaması

---
+++ Collection/TelefonRehberi.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class TelefonRehberi
+    {
+        static void Main(string[] args)
+        {
+            if(!File.Exists("../../veriler.txt"))
+            {
+                Console.WriteLine("Dosya Bulunamadı");
+                Console.ReadLine();
+                return;
+            }
+
+            string dosya_yolu = "../../veriler.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            SortedDictionary<string, SortedDictionary<string, string>> rehber = new SortedDictionary<string, SortedDictionary<string, string>>();
+
+            string yazi = sr.ReadLine();
+
+            while(yazi != null)
+            {
+                string[] dizi = yazi.Split('|');
+                if (rehber.ContainsKey(dizi[1]))
+                {
+                    rehber[dizi[1]].Add(dizi[0], dizi[2]);
+                }
+                else
+                {
+                    rehber.Add(dizi[1], new SortedDictionary<string, string>());
+                    rehber[dizi[1]].Add(dizi[0], dizi[2]);
+                }
+                yazi = sr.ReadLine();
+            }
+
+            sr.Close();
+            fs.Close();
+
+            foreach(var obje in rehber)
+            {
+                Console.WriteLine(obje.Key + " : ");
+
+                foreach (var bilgiler in obje.Value)
+                {
+                    string txt = "\t" + bilgiler.Key;
+                    for(int i = 0; i < 15-(bilgiler.Key.Length); i++)
+                    {
+                        txt += " ";
+                    }
+                    txt += " - ";
+                    for (int i = 0; i < 20 - (bilgiler.Value.Length); i++)
+                    {
+                        txt += " ";
+                    }
+                    Console.WriteLine(txt + bilgiler.Value);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # C-ile-Koleksiyonlar-Sorted-List-Telefon-Rehberi
-C# ile Koleksiyonlar Sorted List kullanarak Dosyadan Okuduğumuz Verilerle Telefon Rehberi Uygulaması Yapımı
+
+##C# ile Koleksiyonlar Sorted List kullanarak Dosyadan Okuduğumuz Verilerle Telefon Rehberi Uygulaması Yapımı
+
+Kayıtları metin dosyasında tutulan bir telefon rehberi üzerinde bazı sıralama ve listeleme işlemlerinin yapılması 
+istenmektedir. Kişi isimleri, bu kimselerin şehirleri ve telefon numaralarının bulunduğu bu dosyanın şablonu aşağıda görülmektedir.
+
+Tahir | Konya | 332-223-3336
+Selahattin | Ankara | 312-123-4567
+Okan | İzmir | 232-147-2589
+Ayşe | Ankara | 312-258-3698
+Deniz | Konya | 332-369-7896
+
+Hazırlayacağınız programın rehberdeki şehir isimlerini alfabetik olarak listelemesi ve bunun altında kişi isimlerini yine 
+alfabetik sırada telefon numaralarıyla birlikte göstermesi gerekmektedir. Bu problem birçok farklı yolla çözülebilir. 
+Ancak burada istenen herhangi bir ekstra sıralama işlemi yapmadan; dosyadan okuma esnasında veri yapısının oluşturulmasıyla 
+problemin çözülmesidir. Alınması gereken ekran çıktısı örneği aşağıdaki gibi olmadır:
+
+Ankara :
+	Ayşe - 312-258-3698
+	Selahattin - 312-123-4567
+İzmir :
+	Okan - 232-147-2589
+Konya :
+	Deniz - 332-369-7896
+	Tahir - 332-223-3336
+	
+	
+Bu amaçla dengeli-arama-ağacı veri yapısına dayanan SortedDictionary<K,T> koleksiyonunu hakkında araştırma yapmanız ve problemi 
+bu koleksiyon ile çözmeniz istenmektedir.
+
+SortedDictionary<string, SortedDictionary<string, string>>
